aa7765f1162fc79b35471d9451d49af5593d803a
Bety Martinez <trism460@gmail.com>
2022-10-13 17:21:35 -0500

    Generate components

--- pelea-designs/src/app/app.module.ts
+++ pelea-designs/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductComponent } from './components/product/product.component';
+import { CartComponent } from './components/cart/cart.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { NotificationsComponent } from './components/notifications/notifications.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { MenuHomeComponent } from './components/menu-home/menu-home.component';
+import { MenuGeneralComponent } from './components/menu-general/menu-general.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductComponent,
+    CartComponent,
+    ProfileComponent,
+    NotificationsComponent,
+    LoginComponent,
+    SignUpComponent,
+    MenuHomeComponent,
+    MenuGeneralComponent
   ],
   imports: [
     BrowserModule,
