fdcacf30a5ea93707738ccc49f3a95bab56ab527
Bety Martinez <trism460@gmail.com>
2022-10-13 17:30:00 -0500

    Generate routes

--- pelea-designs/src/app/app-routing.module.ts
+++ pelea-designs/src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './components/cart/cart.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { MenuGeneralComponent } from './components/menu-general/menu-general.component';
+import { MenuHomeComponent } from './components/menu-home/menu-home.component';
+import { NotificationsComponent } from './components/notifications/notifications.component';
+import { ProductComponent } from './components/product/product.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'cart', component: CartComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'menu-general', component: MenuGeneralComponent },
+  { path: 'menu-home', component: MenuHomeComponent },
+  { path: 'notifications', component: NotificationsComponent },
+  { path: 'product', component: ProductComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'sign-up', component: SignUpComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
